d55cf4d54dcc5b39b5235bd88e933ed8246b408c
chaudharydeepanshu <deepanshuc2141@outlook.com>
2022-01-12 18:29:22 +0530

    MediaQuery.of(context).orientation not required anymore as we can now use AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize instead of AdSize.getAnchoredAdaptiveBannerAdSize to get correct orientation automatically

--- lib/ads/anchored_adaptive_banner_adSize.dart
+++ lib/ads/anchored_adaptive_banner_adSize.dart
@@ -3,10 +3,13 @@ import 'package:flutter/material.dart';
 
 Future<AnchoredAdaptiveBannerAdSize?> anchoredAdaptiveBannerAdSize(
     BuildContext context) async {
-  return await AdSize.getAnchoredAdaptiveBannerAdSize(
-    MediaQuery.of(context).orientation == Orientation.portrait
-        ? Orientation.portrait
-        : Orientation.landscape,
-    MediaQuery.of(context).size.width.toInt(),
-  );
+  //commented out as google_mobile_ads version 0.13.6 supports AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize() to support getting an AnchoredAdaptiveBannerAdSize in the current orientation.
+  // return await AdSize.getAnchoredAdaptiveBannerAdSize(
+  //   MediaQuery.of(context).orientation == Orientation.portrait
+  //       ? Orientation.portrait
+  //       : Orientation.landscape,
+  //   MediaQuery.of(context).size.width.toInt(),
+  // );
+  return await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
+      MediaQuery.of(context).size.width.toInt());
 }
